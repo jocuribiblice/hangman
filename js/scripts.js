9fcdac6cf1dc4fad04f5c518587125df7b190ead
GhitaB <ghitabizau27@gmail.com>
2021-05-10 10:43:35 +0300

    Improve game.

--- js/scripts.js
+++ js/scripts.js
@@ -41,7 +41,7 @@ $(document).ready(function() {
 
 
   function game_over() {
-    $("p#status").text("Ai pierdut. Cuvântul era: " + secret_word);
+    $("p#status").text("Ai pierdut. Cuvântul era: " + secret_word.toUpperCase());
     game_is_finished = true;
   }
 
